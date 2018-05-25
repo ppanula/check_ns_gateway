ad9cddba843b883a71ed4090159956af853c9e7f
Pekka Panula <pekka.panula@sofor.fi>
2018-05-25 15:23:13 +0300

    Version 2. Added support for HTTPS.

--- README.md
+++ README.md
@@ -1,13 +1,18 @@
 Nagios/Naemon check plugin:
 Checks Netscaler gateway vserver status.
 
-Tested on CentOS 6.x, Python v2.6.6
+Tested on CentOS 6.x, SCL Python v2.7
 
 Requirements: 
+- SSL options requires Python 2.7+ to work correctly, this assumes that SCL Python27 is installed.
+```
+  yum install -y centos-release-SCL
+  yum install -y python27 
+```
 
-Citrix nitro-python api installed, download it from inside Netscaler appliance
-located: /var/netscaler/nitro/nitro-python.tgz
-unpack and install it: ```python setup.py install```
+- Citrix nitro-python api installed, download it from inside Netscaler appliance
+  located: /var/netscaler/nitro/nitro-python.tgz
+  unpack and install it: ```python setup.py install``` or install with SCL: ```scl enable python27 "python setup.py install"```
 
 nagios command template:
   - set user macros USER47 and USER48 in resource.cfg, as username and password for Netscaler
@@ -16,6 +21,18 @@ nagios command template:
  define command {
         command_name    check_netscaler_gateway
         command_line    $USER1$/check_ns_gateway.py --host $HOSTADDRESS$ --user '$USER47$' --password '$USER48$' --gatewayname $ARG1$ 
+ }
+ ```
+ Templates for HTTPS with/without cert verification, using SCL Python v2.7:
+ ```
+ define command {
+        command_name    check_netscaler_gateway_ssl
+        command_line    scl enable python27 "$USER1$/check_ns_gateway.py --ssl --host $HOSTADDRESS$ --user $USER47$ --password $USER48$ --gatewayname $ARG1$ "
+ }
+
+ define command {
+        command_name    check_netscaler_gateway_ssl_noverify
+        command_line    scl enable python27 "$USER1$/check_ns_gateway.py --ssl --nosslverify --host $HOSTADDRESS$ --user $USER47$ --password $USER48$ --gatewayname $ARG1$ "
  }
  ```
  
--- check_ns_gateway.py
+++ check_ns_gateway.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
 # 
 # Checks Netscaler gateway vserver status
-# (c) 2015 Pekka Panula / Sofor Oy
+# (c) 2015-2018 Pekka Panula / Sofor Oy
 # Licence: GPLv2 or later
-# Version: 1.0
-# Dated: 4.9.2015
-# Tested on CentOS 6.x, Python v2.6.6
+# Version: 2.0
+# Dated: 25.5.2018
+# Tested on CentOS 6.x, SCL Python v2.7 ! 
+# WARNING: Python v2.6 has SSL warning even if you use --nosslverify!
 #
-# Requirements: Citrix nitro-python api installed, download it from inside Netscaler appliance
-# located: /var/netscaler/nitro/nitro-python.tgz
-# unpack and install it: python setup.py install
+# Requirements: - SSL options requires Python 2.7+ to work correctly, this assumes that SCL Python27 is installed. 
+#               - Citrix nitro-python api installed, download it from inside Netscaler appliance
+#                 located: /var/netscaler/nitro/nitro-python.tgz
+#                 unpack and install it: python setup.py install
 #
 # possible states:
 #  "UP"
@@ -30,12 +32,26 @@
 #        command_name    check_netscaler_gateway
 #        command_line    $USER1$/check_ns_gateway.py --host $HOSTADDRESS$ --user '$USER47$' --password '$USER48$' --gatewayname $ARG1$ 
 # }
+# 
+# If you want SSL version, with cert verification:
+# define command {
+#        command_name    check_netscaler_gateway_ssl
+#        command_line    scl enable python27 "$USER1$/check_ns_gateway.py --ssl --host $HOSTADDRESS$ --user $USER47$ --password $USER48$ --gatewayname $ARG1$ "
+# }
+# 
+# If you want SSL version, without cert verification:
+# define command {
+#        command_name    check_netscaler_gateway_ssl_noverify
+#        command_line    scl enable python27 "$USER1$/check_ns_gateway.py --ssl --nosslverify --host $HOSTADDRESS$ --user $USER47$ --password $USER48$ --gatewayname $ARG1$ "
+# }
 #
 
 import sys
 import time
 import sys
 import argparse
+import os
+import urllib3
 from pprint import pprint
 
 # Citrix's Nitro API
@@ -49,6 +65,8 @@ def print_r(the_object):
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description='Check Netscaler gateway vserver status')
 	parser.add_argument('--host', metavar='HOSTNAME', required=True, help='Netscaler hostname')
+	parser.add_argument('--ssl', action='store_true', default=False, required=False, help='Use HTTPS', dest='useHTTPS')
+	parser.add_argument('--nosslverify', action="store_true", default=False, help='turn SSL verify off', dest='nosslverify')
 	parser.add_argument('--user', metavar='USERNAME', default='nsroot', help='Netscaler username')
 	parser.add_argument('--password', metavar='PASSWORD', default='nsroot', help='Netscaler password')
 	parser.add_argument('--gatewayname', metavar='GATEWAY', required=True, help='Name of Gateway vserver; Use ALL to get a list of all gateway vservers')
@@ -56,9 +74,23 @@ if __name__ == "__main__":
 	args = parser.parse_args()
 
 	try:
-                client = nitro_service(args.host,"http")
+                if args.useHTTPS:
+			client = nitro_service(args.host,"https")
+
+			if args.nosslverify:
+				urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+				os.environ['CURL_CA_BUNDLE']=""
+				os.environ['PYTHONWARNINGS']="ignore:Unverified HTTPS request"
+				client.certvalidation = False 
+				client.hostnameverification = False
+
+		else:
+			client = nitro_service(args.host,"http")
+
                 client.set_credential(args.user,args.password)
-                client.timeout = 900
+                client.timeout = 310
+
+		client.login()
 
 		if args.gatewayname == "ALL":
                         gwList = vpnvserver().get(client)
